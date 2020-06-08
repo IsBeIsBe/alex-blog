ffdc03289c11489ba52e9dc2d38a4877108192a9
Isobel <isobel.betteridge@gmail.com>
2020-06-08 16:32:28 +0100

    Created basic components to display blogposts resource

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import client from './api/client';
+
+
+
+let responseLogger = function (data, response) {
+  // console.log(data)
+  // console.log(response)
+}
+
+// let recordBlogPosts = function (data, response){
+//   state.blogPosts = response
+// }
+
+// client.methods.getBlogPosts(recordBlogPosts);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Blog />
       </header>
     </div>
   );
+}
+
+function Post(post, key) {
+  return (<li key={key}>{post.post.Content}</li>)
+}
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  recordBlogPosts(data, response) {
+    this.setState({ posts: data });
+  }
+
+  record = (data, response) => {
+    this.recordBlogPosts(data, response)
+  }
+
+  componentDidMount() {
+    client.methods.getBlogPosts(this.record);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.posts.map((value) => {return <Post post={value} key={value.id}/>})}
+      </ul>
+    )
+  }
 }
 
 export default App;
