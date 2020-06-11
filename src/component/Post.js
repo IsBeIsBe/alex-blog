ff835fa0095068a26bf64f47c0130b1a4bd268a7
Isobel <isobel.betteridge@gmail.com>
2020-06-11 14:08:06 +0100

    formatted Post to include rows/columns

--- src/component/Post.js
+++ src/component/Post.js
@@ -2,13 +2,31 @@ import React, { useState } from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function formatDate(rawDate) {
+    let publishedDate = new Date(rawDate);
+    let dateFormat = new Intl.DateTimeFormat("en", {year: "numeric", month: "short", day: "2-digit"});
+
+    return dateFormat.format(publishedDate);
+}
+
 function Post(post, key) {
-    return (
-  
+    return (  
       <div>
-        <div>
+        <div className="row">
+        <div className="col-sm text-left">
           <h2>{ post.post.Title }</h2>
         </div>
+        </div>
+        <hr/>
+        <div className="row">
+            <div className="col-sm text-left text-muted">
+                { post.post.Author }
+            </div>
+            <div className="col-sm text-right text-muted">
+                { formatDate(post.post.Published) }
+            </div>
+        </div>
+        <hr/>
         <div className="text-justify">
           { post.post.Content }
         </div>
