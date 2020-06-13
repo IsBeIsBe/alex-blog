19cc57faa796a306a83b1431eb17e664ede86f4b
Isobel <isobel.betteridge@gmail.com>
2020-06-13 13:54:23 +0100

    refactored Constants.js to remove repeated code

--- src/Constants.js
+++ src/Constants.js
@@ -1,19 +1,9 @@
-const prod = {
-    url: {
-        API_URL: "https://shrouded-reaches-27000.herokuapp.com/",
-        API_URL_BLOGPOSTS: "https://shrouded-reaches-27000.herokuapp.com/blog-posts",
-        API_URL_BIO: "https://shrouded-reaches-27000.herokuapp.com/bio",
-        API_URL_CONTACTINFORMATION: "https://shrouded-reaches-27000.herokuapp.com/contact-information"
-    }
-};
+let config = {
+    BLOGPOSTS_PATH: "blog-posts",
+    BIO_PATH: "bio",
+    CONTACTINFORMATION_PATH: "contact-information"
+}
 
-const dev = {
-    url: {
-        API_URL: "http://localhost:1337/",
-        API_URL_BLOGPOSTS: "http://localhost:1337/blog-posts",
-        API_URL_BIO: "http://localhost:1337/bio",
-        API_URL_CONTACTINFORMATION: "http://localhost:1337/contact-information"
-    }
-};
+config.API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:1337/" : "https://shrouded-reaches-27000.herokuapp.com/"
 
-export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export default config;
--- src/api/client.js
+++ src/api/client.js
@@ -1,11 +1,11 @@
-import { config } from '../Constants'
+import config from '../Constants'
 
 var Client = require('node-rest-client').Client;
 
 var client = new Client();
 
-client.registerMethod("getBlogPosts", config.url.API_URL_BLOGPOSTS, "GET");
-client.registerMethod("getBio", config.url.API_URL_BIO, "GET")
-client.registerMethod("getContactInformation", config.url.API_URL_CONTACTINFORMATION, "GET")
+client.registerMethod("getBlogPosts", config.API_URL + config.BLOGPOSTS_PATH, "GET");
+client.registerMethod("getBio", config.API_URL + config.BIO_PATH, "GET")
+client.registerMethod("getContactInformation", config.API_URL + config.CONTACTINFORMATION_PATH, "GET")
 
 export default client;
