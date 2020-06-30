d43be7bf25300a45d08387c1a68f522c31c3ec12
Isobel Betteridge <46503459+IsBeIsBe@users.noreply.github.com>
2020-06-30 17:56:16 +0100

    Implement home page (#16)
    * Accordion implemented
    
    * removed console.logs
    
    * removed unused imports
    
    * implemented home page
    
    * implemented home page
    
    * code style fixes

---
+++ src/component/Homepage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../App.css';
+import client from '../api/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: null
+        }
+    }
+
+    retrieveHomepage(data, response) {
+        this.setState({ content: data.Content })
+    }
+
+    componentDidMount() {
+        client.methods.getHomepage(this.retrieveHomepage.bind(this))
+    }
+
+    render() {
+        return (
+            (
+                <div>
+                    <div className="row">
+                        <div className="col-sm text-left">
+                            <h2>Carousel GOES HERE</h2>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="text-justify">
+                        { this.state.content }
+                    </div>
+                </div>
+            )
+        )
+    }
+}
+
+export default Homepage;
